6be2397ca4577e1440fa24d1a68158f7e15617b6
sakard23 <sak_4204@yahoo.com>
2016-06-22 18:46:04 -0500

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+//this is a header line, which tells standard input/output of C compile to allow use of printf command
+#include <stdio.h>  
+
+//here we use main function of C as "main"
+int main (void) 
+{
+ //this line we print hello world using command printf. also, we always give semi-colon after every lines of code
+    printf ("hello world!!"); 
+}
